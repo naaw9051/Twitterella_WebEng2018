d4e9d2536b10ec53f5078471d635e35b23a10627
Nathalie <n.awitor@gmx.de>
2018-06-22 17:38:42 +0200

    Add validate methods to transferobjects

--- src/main/java/webeng/tranferobjects/Message.java
+++ src/main/java/webeng/tranferobjects/Message.java
@@ -2,54 +2,76 @@ package webeng.tranferobjects;
 
 import java.io.Serializable;
 
-public class Message implements Serializable{
+public class Message implements Serializable {
 
 	private int id;
 	private String message;
 	private int likes;
 	private int userID;
 	//private Blob picture;
-	
-	public Message(){}
-	
-	public void setID(int id){
+
+	public Message() {
+	}
+
+	public void setID(int id) {
 		this.id = id;
 	}
-	
-	public void setMessage(String message){
+
+	public void setMessage(String message) {
 		this.message = message;
 	}
-	
-	public void setLikes(int likes){
+
+	public void setLikes(int likes) {
 		this.likes = likes;
 	}
-	
-	public void setUserID(int userID){
+
+	public void setUserID(int userID) {
 		this.userID = userID;
 	}
-	
-	//void setPicture(Blob picture){}
-	
-	public int getID(){
+
+	//public void setPicture(Blob picture){
+	//this.picture = picture;
+	//}
+
+	public int getID() {
 		return this.id;
 	}
-	
-	public String getMessage(){
+
+	public String getMessage() {
 		return this.message;
 	}
-	
-	public int getLikes(){
+
+	public int getLikes() {
 		return this.likes;
 	}
-	
+
 	public int getUserID() {
 		return this.userID;
 	}
-	
-	//Blob getPicture(){}
-	
-	//TODO
-	public boolean validate(){
-		return false;
+
+	//public Blob getPicture(){
+	//	return this.picture;
+	//}
+
+	public boolean validate() {
+		boolean valid = true;
+
+		if (this.id <= 0 || !isNumeric("" + this.id)) {
+			valid = false;
+		}
+		if (this.message == null || this.message.equals(" ")) {
+			valid = false;
+		}
+		if (this.likes < 0) {
+			valid = false;
+		}
+		if (this.userID <= 0 || !isNumeric("" + this.id)) {
+			valid = false;
+		}
+		return valid;
+	}
+
+	private boolean isNumeric(String str) {
+		return str.matches("-?\\d+(\\.\\d+)?");
 	}
 }
--- src/main/java/webeng/tranferobjects/User.java
+++ src/main/java/webeng/tranferobjects/User.java
@@ -2,40 +2,54 @@ package webeng.tranferobjects;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+public class User implements Serializable {
 	private int id;
 	private String name;
 	private String password;
-	
-	public User(){}
-	
-	public void setID(int id){
+
+	public User() {
+	}
+
+	public void setID(int id) {
 		this.id = id;
 	}
-	
-	public void setName(String name){
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public void setPassword(String password){
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	public int getID(){
+
+	public int getID() {
 		return this.id;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return this.name;
 	}
-	
-	public String getPassword(){
+
+	public String getPassword() {
 		return this.password;
 	}
 
-	//TODO
-	public boolean validate(){
-		boolean valid = false;
+	public boolean validate() {
+		boolean valid = true;
+
+		if (this.id == 0 || !isNumeric("" + this.id)) {
+			valid = false;
+		}
+		if (this.name == null || this.name.contains(" ")) {
+			valid = false;
+		}
+		if (this.password == null || this.password.contains(" ")) {
+			valid = false;
+		}
 		return valid;
+	}
+
+	private boolean isNumeric(String str) {
+		return str.matches("-?\\d+(\\.\\d+)?");
 	}
 }
